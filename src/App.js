8f9d7df2b84370aaefc721978bc9fcb6e0e42248
Yoojeen <sendtoeugeen@gmail.com>
2021-07-03 12:44:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ImageSlider from './components/ImageSlider'
+import sliderData from './components/sliderData'
+import sliderData2 from './components/sliderData2'
+import sliderData3 from './components/sliderData3'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){ 
+  return  (
+    <React.Fragment>
+    <div className="bikes">
+      {/* passing option for dots to be shown, and some html for slides */}
+      <ImageSlider dots={true} sliderData={sliderData} />
     </div>
-  );
+    <section className="section-text-info">
+      <h1>Slider without dots</h1>
+    <div className="text-info">
+      {/* passing option for dots not to be shown, and some html for slides */}
+      <ImageSlider dots={false} sliderData={sliderData2} />
+    </div>
+    </section>
+    <section className="section-clients-say">
+      <h1>Another without dots</h1>
+    <div className="clients-say">
+      {/* passing option for dots not to be shown, and some html for slides */}
+      <ImageSlider dots={false} sliderData={sliderData3} />
+    </div>
+    </section>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
